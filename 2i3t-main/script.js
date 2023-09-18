2ba4d72e77b8c6bc0e8db8eafcb0b32309e12c44
bibi <114074984+bibirodri@users.noreply.github.com>
2023-09-18 10:55:42 -0300

    Add files via upload

---
+++ 2i3t-main/script.js
@@ -0,0 +1,37 @@
+function alo_mundo(){
+    alert ("projeto iniciado! alo!")
+}
+
+
+escrever = msg => alert(msg);
+soma = (a,b) => a + b;
+sub = (a,b)  => a - b;
+mult = (a,b) => a * b;
+div = (a,b)  => a / b;
+raiz = a => Math.sqrt(a);
+equacao2Grau = (a,b,c) => {
+    let delta = sub(mult(b,b),mult(4,mult(a,c)));
+    if (delta < 0) return " Não possui raiz Real";
+    if (delta ==0) return  "x1 = x2 " + div(-b,mult(2,a));
+    return "x1 =" + div(soma(-b,raiz(delta)),mult(2,a)) + "x2 ="
+    + div(sub(-b,raiz(delta)),mult(2,a));
+}
+
+const mostrar_resultado = document.getElementById("resultado").value = resultado;
+
+let a = 0
+let b = 0
+let resultado = 0;
+let executar = "";
+
+calcular = () =>{
+    if (executar = "soma") soma (a,b);
+    if (executar = "sub") sub (a,b);
+    if (executar = "/") div (a,b);
+    if (executar = "*") mult (a,b);
+}
+{
+    operacao = op => executar = op;
+    digitando = (num)=>
+         alert(num)
+}
